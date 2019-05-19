a09523759cdddebe77ff4b6080d07d1bc4afc1eb
minhthuy98nbk <42841701+minhthuy98nbk@users.noreply.github.com>
2019-05-19 10:35:17 +0700

    Add files via upload

---
+++ mega_hardwareSerial.ino
@@ -0,0 +1,481 @@
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+#include <Adafruit_Fingerprint.h>
+#include "Keypad.h"
+SoftwareSerial mySerial(12, 13);           // pin fingerprint
+LiquidCrystal lcd(41, 39, 37, 35, 33, 31);    // pin lcd
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+#define LOCK_PIN 4                        // pin lock
+#define BELL_PIN 5                       // pin buzzer
+#define BUTTON 6                         // pin button
+
+// buzzer
+#define NOTE_G3  196
+#define NOTE_A3  220
+#define NOTE_B3  247
+#define NOTE_C4  262
+int melody_notFound[] = {NOTE_C4, 0, 0, 0, 0, 0};                   // melody buzzer not found
+int noteDurations_notFound[] = {1, 8, 8, 4, 4, 4};                  // length of the buzzer sounds not found
+int melody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0};    // melody buzzer
+int noteDurations[] = {4, 8, 8, 4, 4, 4};                           // length of the buzzer sounds
+
+//keypad
+const byte rows = 4;
+const byte columns = 4;
+char keys[rows][columns] =
+{
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'},
+};
+byte rowPins[rows] = {46, 47, 48, 49};
+byte columnPins[columns] = {50, 51, 52, 53};
+char password[] = {'0', '0', '0', '0'};
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, columnPins, rows, columns);
+
+uint8_t id;
+
+int state = 0;
+char key = 0;
+/*------------------ SETUP -------------------*/
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  pinMode(LOCK_PIN, OUTPUT);
+  pinMode(BELL_PIN, OUTPUT);
+  pinMode(BUTTON, INPUT);
+
+  lcd.begin(16, 2);     // set the data for the lcd port
+  finger.begin(57600);  // set the data rate for the sensor serial port
+
+  // check circuit
+  Display("SMART LOCK", 0);
+  if (finger.verifyPassword()) {
+    Display("correct circuit", 1);
+    delay(1000);
+  }
+  else {
+    Display("wrong circuit", 1);
+    while (1) {
+      delay(1);
+    }
+  }
+  Display("SMART LOCK", 0);
+  Display("waiting...", 1);
+}
+
+/*------------------ LOOP -------------------*/
+
+void loop() {
+  char temp = keypad.getKey();
+  if ((int)keypad.getState() ==  PRESSED)
+  {
+    if (temp != 0) key = temp;
+    if (key == 'A') state = 1;
+    else if (key == '*') state = 2;
+    else if (key == 'C') state = 3;
+    else if (key == 'D') state = 4;
+  }
+
+  if (state == 0) { // check fingerprint
+    getFingerprintIDez();
+  }
+  else if (state == 1) { // add fingerprint
+    if (enterPassword() == true) getFingerprintEnroll();
+    Display("SMART LOCK", 0);
+    Display("waiting...", 1);
+    state = 0;
+  }
+  else if (state == 4) { // remove all fingerprint
+    if (enterPassword() == true) deleteAllFingerprint();
+    state = 0;
+  }
+  else if (state == 2) { // remove one fingerprint
+    if (enterPassword() == true) deleteOneFingerprint();
+    state = 0;
+  }
+  else if (state == 3) { // print the number of fingerprint
+    printNumberFingerPrint();
+    state = 0;
+  }
+}
+
+/*------------------ ENTER PASSWORD -------------------*/
+
+bool enterPassword()
+{
+  Display("Enter password", 0);
+  char a[5];
+  int cnt = 0;
+  while (cnt <= 3)
+  {
+    char temp = keypad.getKey();
+    if (temp)
+    {
+      a[cnt] = temp;
+      lcd.setCursor(cnt, 1);
+      lcd.print(temp);
+      delay(100);
+      lcd.setCursor(cnt, 1);
+      lcd.print("*");
+      cnt++;
+    }
+  }
+  for (int i = 0; i < 4; i++)
+  {
+    if (a[i] != password[i])
+    {
+      Display("Wrong password", 0);
+      displayMainScreen();
+      return false;
+    }
+  }
+  Display("Correct password", 0);
+  delay(2000);
+  return true;
+}
+
+/*------------------ DISPLAY MAIN SCREEN -------------------*/
+
+void displayMainScreen()
+{
+  delay(2000);
+  Display("SMART LOCK", 0);
+  Display("waiting...", 1);
+}
+
+/*------------------ DISPLAY -------------------*/
+
+void Display(String msg, int pos)
+{
+  if (pos == 0) lcd.clear();
+  lcd.setCursor(0, pos);
+  lcd.print(msg);
+  Serial.println(msg);
+}
+
+/*------------------ BELL -------------------*/
+
+void bell (int melody[], int noteDurations[]) {
+  pinMode(BELL_PIN, OUTPUT);
+  for (int thisNote = 0; thisNote < 6; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BELL_PIN, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(BELL_PIN);
+  }
+}
+
+/*------------------ UNLOCK -------------------*/
+
+void unlock() {
+  Display("openning...", 1);
+  bell(melody, noteDurations);
+  digitalWrite(LOCK_PIN, HIGH);
+  delay(2000);
+  Display("closing...", 1);
+  bell(melody, noteDurations);
+  digitalWrite(LOCK_PIN, LOW);
+}
+
+/*------------------ SEND DATA -------------------*/
+
+void sendData(int x){
+  Serial.print("Gui : ");
+  Serial.print("#");
+  Serial.print(x); 
+  Serial.print("@");
+  Serial.print("\n"); 
+}
+
+/*------------------ GET DATA -------------------*/
+
+int getData(){
+  int start_flag = 0;
+  int s = 0;
+  while (1) {
+    while (Serial.available()) {
+      int x = Serial.read();
+      if (x=='#'){
+        s=0;
+        start_flag = 1;
+      } else if (start_flag == 1){
+        if (x=='@'){
+          start_flag = 0;
+          return s;
+        } else {
+          s=s*10+(x-48);
+        }
+      } 
+    }
+  }
+}
+
+/*------------------ NUMBER OF FINGERPRINT-------------------*/
+
+void printNumberFingerPrint()
+{
+  Display("SMART LOCK", 0);
+  uint8_t id = numberFingerPrint();
+  Display((String)(id), 1);
+  if (id <= 1) lcd.print(" template - ");
+  else lcd.print(" templates - ");
+  lcd.print(getIdMax());
+  displayMainScreen();
+}
+
+int numberFingerPrint(){
+  finger.getTemplateCount();
+  uint8_t id = finger.templateCount;
+  return id;
+}
+
+/*------------------ CHECK FINGERPRINT-------------------*/
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK) return -1;
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK) return -1;
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)
+  {
+    Display("Not found!", 1);
+    sendData(0);
+    bell(melody_notFound, noteDurations_notFound);
+    displayMainScreen();
+    return -1;
+  }
+
+  // found a match!
+  sendData(3);
+  delay(1000);
+  sendData((int)(finger.fingerID));
+  lcd.setCursor(0, 0);
+  lcd.print("Hello ID ");
+  lcd.print(finger.fingerID);
+  Serial.print("Chào ID ");
+  Serial.print(finger.fingerID);
+  unlock();
+  displayMainScreen();
+  return finger.fingerID;
+}
+/*------------------ DELETE ALL FINGERPRINT-------------------*/
+
+void deleteAllFingerprint() {
+  Display("DELETING All ... ", 0);
+  sendData(4);
+  finger.emptyDatabase();
+  delay(2000);
+  displayMainScreen();
+  return;
+}
+
+/*------------------ GET ID MAX -------------------*/
+
+int getIdMax()
+{
+  sendData(5);
+  int x = getData();
+  Serial.print("ID MAX : ");
+  Serial.println(x);
+  return x;
+}
+
+/*------------------ GET ID NUMBER FROM KEYPAD -------------------*/
+
+uint8_t getNumber (){
+  uint8_t id = 0;
+  while (1)
+  {
+    char temp = keypad.getKey();
+    if (temp)
+    {
+      lcd.print(temp);
+      if (temp == '#') return id; 
+      else id = id*10 + uint8_t(temp-'0');
+    }    
+  }
+}
+
+/*------------------ DELETE ONE FINGERPRINT-------------------*/
+void deleteOneFingerprint() {
+  Display("DELETE FINGERPRINT ... ", 0);
+  Display("Enter id: ", 1);
+  uint8_t id = getNumber();
+  int idMax = getIdMax();
+  Serial.println("ID to delete : ");
+  Serial.println(id);
+  Serial.println("num : ");
+  Serial.println(numberFingerPrint());
+  Serial.println("MAX : ");
+  Serial.println(idMax);
+  
+  delay(1000);
+  if (id <= idMax)
+  {
+    sendData(2);
+    sendData((int)(id));
+    deleteFingerprint(id);
+    Display("Deleting id ", 1);
+    lcd.print(id);
+  }
+  else Display("id undefined! ", 1);
+  displayMainScreen();
+}
+
+uint8_t deleteFingerprint(uint8_t id) {
+  uint8_t p = -1;
+
+  p = finger.deleteModel(id);
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Deleted!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not delete in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.print("Unknown error: 0x"); Serial.println(p, HEX);
+    return p;
+  }
+}
+
+/*--------------- ADD FINGERPRINT ----------------------*/
+uint8_t getFingerprintEnroll() {
+  int p = -1;
+  Display("ADD FINGERPRINT", 0);
+  int idMax = getIdMax();
+  id = idMax + 1;
+  Serial.println("ID to add : ");
+  Serial.println(id);
+  Serial.println("num : ");
+  Serial.println(numberFingerPrint());
+  Serial.println("MAX : ");
+  Serial.println(idMax);
+  if (idMax >= 162)
+  {
+    Display("full memory", 1);
+    displayMainScreen();
+    return p;
+  }
+  Display("waiting...", 1);
+  while (p != FINGERPRINT_OK)
+  {
+    p = finger.getImage();
+    switch (p)
+    {
+      case FINGERPRINT_OK:
+        Display("The first time", 0);
+        Display("Success!", 1);
+        bell(melody, noteDurations);
+        break;
+      case FINGERPRINT_NOFINGER:
+        break;
+      case FINGERPRINT_PACKETRECIEVEERR:
+      case FINGERPRINT_IMAGEFAIL:
+      default:
+        Display("The first time", 0);
+        Display("Fail!", 1);
+        bell(melody_notFound, noteDurations_notFound);
+        break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(1);
+  switch (p) {
+    case FINGERPRINT_OK:
+      break;
+    case FINGERPRINT_IMAGEMESS:
+    case FINGERPRINT_PACKETRECIEVEERR:
+    case FINGERPRINT_FEATUREFAIL:
+    case FINGERPRINT_INVALIDIMAGE:
+    default:
+      Display("Convert error", 1);
+      bell(melody_notFound, noteDurations_notFound);
+      displayMainScreen();
+      return p;
+  }
+
+  delay(1000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+  p = -1;
+  Display("ADD FINGERPRINT", 0);
+  Display("again...", 1);
+  while (p != FINGERPRINT_OK)
+  {
+    p = finger.getImage();
+    switch (p)
+    {
+      case FINGERPRINT_OK:
+        Display("The second time", 0);
+        Display("Success!", 1);
+        bell(melody, noteDurations);
+        break;
+      case FINGERPRINT_NOFINGER:
+        break;
+      case FINGERPRINT_PACKETRECIEVEERR:
+      case FINGERPRINT_IMAGEFAIL:
+      default:
+        Display("The second time", 0);
+        Display("Fail!", 1);
+        bell(melody_notFound, noteDurations_notFound);
+        break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      break;
+    case FINGERPRINT_IMAGEMESS:
+    case FINGERPRINT_PACKETRECIEVEERR:
+    case FINGERPRINT_FEATUREFAIL:
+    case FINGERPRINT_INVALIDIMAGE:
+    default:
+      Display("Convert error", 1);
+      bell(melody_notFound, noteDurations_notFound);
+      displayMainScreen();
+      return p;
+  }
+
+  // OK converted!
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Display("Match :)", 0);
+  } else {
+    Display("Not match :(", 0);
+    displayMainScreen();
+    return p;
+  }
+
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    sendData(1);
+    delay(1000);
+    sendData((int)(id));
+    Display("Stored id ", 1);
+    lcd.print(id);
+    bell(melody, noteDurations);
+  } else {
+    Display("Can't stored :(", 0);
+    bell(melody_notFound, noteDurations_notFound);
+  }
+  displayMainScreen();
+  return p;
+}
---
+++ nodemcu_hardwareSerial.ino
@@ -0,0 +1,206 @@
+#include <ESP8266WiFi.h>
+#include <NTPtimeESP.h>
+#include <FirebaseArduino.h>
+
+NTPtime NTPch("ch.pool.ntp.org"); 
+strDateTime dateTime; 
+
+#define FIREBASE_HOST "hanghang-2dc20.firebaseio.com"
+#define FIREBASE_AUTH "aoa8aiOhqtkWioNfg0ADOCInzolTMXrAiLjL9Mez"
+#define WIFI_SSID "Ahihi"
+#define WIFI_PASSWORD "123456789"
+
+String x, DATE, TIME;
+int id, type, count, idMax;
+
+/*--------------------- SETUP --------------------*/
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {}
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
+  
+  // set id max
+  x = "VDK/idMax";
+  idMax = Firebase.getFloat(x);
+  if (idMax == 0) Firebase.setFloat(x,0);
+  else Serial.println (idMax);
+  
+  // set infor stranger
+  addNewInfor(0,"unknow","unknow","unknow");
+  x = "VDK/0/count_history";
+  count = Firebase.getFloat(x);
+  if (count == 0) Firebase.setFloat(x,0);
+  else Serial.println (count);
+}
+
+
+
+/*--------------------- LOOP --------------------*/
+
+void loop() {
+  
+  Serial.print("Nhan : ");
+  type = getNumSerial();
+  Serial.println(type); 
+  dateTime = NTPch.getNTPtime(7.0, 0);
+
+  if (type==5){
+      delay(30);
+      idMax = Firebase.getFloat("VDK/idMax");
+      sendNumSerial(idMax);
+  } else {
+      DATE = gettDate();
+      TIME = gettTime();
+      if (DATE.length()!=10 || TIME.length()!=8) return;
+      Serial.print("DATE : "); Serial.println(DATE); 
+      Serial.print("TIME : "); Serial.println(TIME); 
+      if (type == 0){ // stranger
+          x = "VDK/0/count_history";
+          count = Firebase.getFloat(x)+1;
+          Firebase.setFloat(x,count);
+          setHistory(0,"Try to open door",count);
+          
+      } else if (type==4) { // delete all
+          idMax = Firebase.getFloat("VDK/idMax");
+          Serial.print("idMax :");
+          Serial.println(idMax);
+          for (int i=1; i<=idMax; i++)
+          {
+              x = "VDK/" + (String)(i) + "/count_history";
+              count = Firebase.getFloat(x)+1;
+              Firebase.setFloat(x,count);
+              
+              Serial.print ("Xoa : ");
+              Serial.println (i);
+              x = "VDK/" + (String)(i) + "/infor/status";
+              Firebase.setString(x, "resigned");
+              setHistory(i,"Delete fingerprint",count);
+          }
+          
+      } else {
+          
+          Serial.print("Nhan : ");
+          id = getNumSerial();
+          Serial.println(id);
+          
+          if (type == 1){ // add 
+              addNewInfor(id,"X","X","working");
+              setHistory(id,"Add fingerprint",1);
+              x = "VDK/" + (String)(id) + "/count_history";
+              Firebase.setFloat(x,1);
+              Firebase.setFloat("VDK/idMax",id);
+          
+          } else if (type == 2){ // delete one
+              x = "VDK/" + (String)(id) + "/count_history";
+              count = Firebase.getFloat(x)+1;
+              Firebase.setFloat(x,count);
+              
+              x = "VDK/" + (String)(id) + "/infor/status";
+              Firebase.setString(x, "resigned");
+              setHistory(id,"Delete fingerprint",count);
+          }
+
+          else { // open door
+              x = "VDK/" + (String)(id) + "/count_history";
+              count = Firebase.getFloat(x)+1;
+              Firebase.setFloat(x,count);
+              setHistory(id,"Open door",count);
+              
+          }
+      }
+      // Kiem tra ket noi firebase
+      if (Firebase.failed()) {
+        Serial.print("setting /number failed:");
+        Serial.println(Firebase.error());
+        return;
+      }
+        Serial.println("Sent");
+      }
+}
+
+/*--------------------- ADD NEW --------------------*/
+
+void addNewInfor(int id, String s1, String s2, String s3){
+    Serial.print("addNewInfor");Serial.println(id);
+    x = "VDK/" + (String)(id) + "/infor/fullName";
+    Firebase.setString(x, s1);
+    x = "VDK/" + (String)(id) + "/infor/class";
+    Firebase.setString(x, s2);
+    x = "VDK/" + (String)(id) + "/infor/status";
+    Firebase.setString(x, s3);
+}
+
+/*--------------------- SET HISTORY --------------------*/
+
+void setHistory(int id, String s, int count){
+    Serial.print("setHistory");Serial.println(id);
+    x = "VDK/" + (String)(id) + "/history/" + (String)(count) + "/date";   
+    Firebase.setString(x, DATE);
+    x = "VDK/" + (String)(id) + "/history/" + (String)(count) + "/time";   
+    Firebase.setString(x, TIME);
+    x = "VDK/" + (String)(id) + "/history/" + (String)(count) + "/type";   
+    Firebase.setString(x,s);
+}
+
+/*--------------------- SEND DATA --------------------*/
+void sendNumSerial(int x){
+  Serial.print("Gui : ");
+  Serial.print("#");
+  Serial.print(x); 
+  Serial.print("@");
+  Serial.print("\n"); 
+}
+
+/*--------------------- GET DATE --------------------*/
+
+String gettDate (){
+    DATE = "";
+    if (dateTime.day<10) DATE += "0";
+    DATE += ((String)(dateTime.day)+"/");
+    if (dateTime.month<10) DATE += "0";
+    DATE += ((String)(dateTime.month)+"/"+(String)(dateTime.year));
+    return DATE;
+}
+
+/*--------------------- GET TIME --------------------*/
+String gettTime (){
+    TIME = "";
+    if (dateTime.hour<10) TIME += "0";
+    TIME += ((String)(dateTime.hour)+":");
+    if (dateTime.minute<10) TIME += "0";
+    TIME += ((String)(dateTime.minute)+":");
+    if (dateTime.second<10) TIME += "0";
+    TIME += (String)(dateTime.second);
+    return TIME;
+}
+
+/*--------------------- GET DATA --------------------*/
+int getNumSerial(){
+  int start_flag = 0;
+  int s = 0;
+  while (1) {
+    while (Serial.available()) {
+      int x = Serial.read();
+      if (x=='#'){
+        s=0;
+        start_flag = 1;
+      } else if (start_flag == 1){
+        if (x=='@'){
+          start_flag = 0;
+          return s;
+        } else {
+          s=s*10+(x-48);
+        }
+      } 
+    }
+  }
+}
